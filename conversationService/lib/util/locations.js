8c0c7a87aab112903c372f9a00e6e3ae77d89082
Oran Leiba <leiba.oran@gmail.com>
2018-05-26 18:46:55 +0300

    handling location

--- conversationService/lib/util/locations.js
+++ conversationService/lib/util/locations.js
@@ -8,12 +8,14 @@ const STREET_ADDRESS = 'street_address';
 
 const googleMapsClient = googleMaps.createClient({
   key: API_KEY,
+  language: 'iw',
   Promise: Promise
 });
 
 async function cordsToAddress(cords) {
   try {
-    const response = await googleMapsClient.reverseGeocode({latlng: cords}).asPromise();
+    const latlng = {lat:cords.lat, lng:cords.long};
+    const response = await googleMapsClient.reverseGeocode({latlng: latlng}).asPromise();
     return responseToAddresses(response.json.results);
   } catch (err) {
     console.log(err);
@@ -42,9 +44,10 @@ async function placeToAddress(placeId) {
 }
 
 function responseToAddresses(responseArr) {
+    console.log(JSON.stringify(responseArr));
   const arr = [];
   responseArr.forEach(function(address) {
-    //if (address.types && address.types.includes(STREET_ADDRESS)) {
+    if (address.types && address.types.includes(STREET_ADDRESS)) {
         let lat;
         let lng;
         if (address.geometry && address.geometry.location) {
@@ -52,16 +55,19 @@ function responseToAddresses(responseArr) {
           lng = address.geometry.location.lng;
         }
         arr.push(new Address(lat, lng, address.formatted_address, address.place_id));
-    //}
+    }
   });
   return arr;
 }
 
 //cordsToAddress({lat: 32.06193320000001, lng:34.7700711}).then(console.log);
-cordsToAddress({lat: 40.731, lng:-73.997}).then(console.log);
+//cordsToAddress({lat: 40.731, lng:-73.997}).then(console.log);
 
 
 //strToAddress('Herzl St 20').then(console.log);
+//strToAddress('ראשון לציון').then(console.log);
+
+//strToAddress("ז'בוטינסקי 25 ראשון לציון דירה 5 קומה 3 כניסה 1").then(console.log);
 
 //placeToAddress('ChIJ995Ut0W0AhURHDqLrE662QE').then(console.log);
 

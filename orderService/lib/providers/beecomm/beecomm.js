f33924667980067c118d8d8b13cc90f48fbc844c
eranshpiner <eranshpiner@gmail.com>
2018-11-17 23:37:13 +0200

    updated the beecomm

--- orderService/lib/providers/beecomm/beecomm.js
+++ orderService/lib/providers/beecomm/beecomm.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const https = require('https');
 const qs = require('querystring');
 
 const conf = require('../../../config/conf');
@@ -14,9 +15,9 @@ const orderCenterResource = `/api/v${providerConf.apiVersion}/services/orderCent
 const pushOrderResource = orderCenterResource + '/pushOrder';
 
 // beecomm access token
-const access_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1Mjc1ODU4OTU2NDUsImNsaWVudCI6eyJfaWQiOiI1YWQ3MjUxYjBkZmJlMGEwOTAyOGU0ZjciLCJjbGllbnRfbmFtZSI6InZlbm9zIiwiaGViTmFtZSI6IteV16DXldehIiwidHlwZSI6ImRlbGl2ZXJ5IiwiaXNBY3RpdmUiOnRydWUsInJlZ2lzdHJhdGlvbkRhdGUiOiIxOC0wNC0yMDE4IDEzOjU5OjM5IiwibGV2ZWwiOjUsInJvbGUiOiJhcHAtY2xpZW50IiwiY2xpZW50X2lkIjoiUzFrR0NubTloa0tmaDBwbG9ua1ZCbUFMSnZ6R2NWVzFhMHFlOEJPMFBUZ1dUUDBnbWlUTjMwMFNtV3BadHpNeCIsImNsaWVudF9zZWNyZXQiOiI1RjZ6c2pVNkhOVlA2MXVxM1VERHpsM1RPQU9paFh6R3lCRVVqbU8wZGowc2lLMUxnQndQdjBjTWswZDJRTTlWIn19.mvTYMNe7GebMAxAIsjw44l5UJx7ZLcLMcmZz06beYVE';
+const access_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NDI3NDUxODY5NDIsImNsaWVudCI6eyJfaWQiOiI1YWQ3MjUxYjBkZmJlMGEwOTAyOGU0ZjciLCJjbGllbnRfbmFtZSI6InZlbm9zIiwiaGViTmFtZSI6IteV16DXldehIiwidHlwZSI6ImRlbGl2ZXJ5IiwiaXNBY3RpdmUiOnRydWUsInJlZ2lzdHJhdGlvbkRhdGUiOiIxOC0wNC0yMDE4IDEzOjU5OjM5IiwibGV2ZWwiOjUsInJvbGUiOiJhcHAtY2xpZW50IiwiY2xpZW50X2lkIjoiUzFrR0NubTloa0tmaDBwbG9ua1ZCbUFMSnZ6R2NWVzFhMHFlOEJPMFBUZ1dUUDBnbWlUTjMwMFNtV3BadHpNeCIsImNsaWVudF9zZWNyZXQiOiI1RjZ6c2pVNkhOVlA2MXVxM1VERHpsM1RPQU9paFh6R3lCRVVqbU8wZGowc2lLMUxnQndQdjBjTWswZDJRTTlWIn19.aMqYlmgXEd8LmXwPSOG-ENB9c6Q1fxRbfs0l7tj6IN4';
 
-function transformOrder(source) {
+function transformOrder(source, pos) {
   // validate that source odrer json is according to the internal data-model
   let result = validator.validateInternalOrder(source);
   if (!result) {
@@ -25,20 +26,23 @@ function transformOrder(source) {
   }
 
   const target = {
-    branchId: source.brandLocationId,
+    branchId: pos.posId,
     orderInfo: {
-      orderType: 1,
+      orderType: 2,
       dinners: 2,
-      discountSum: '',
-      outerCompId: 0,
-      outerCompOrderId: '',
+      discountSum: 0.0,
+      outerCompId: 63,
+      outerCompOrderId: 'venos102',
       arrivalTime: '',
       remarks: source.remarks,
       firstName: source.orderOwner.firstName,
       lastName: source.orderOwner.lastName,
       phone: source.orderOwner.phone,
       email: source.orderOwner.email,
-      payments: [],
+      payments: [{
+        paymentType: 2,
+        paymentSum: source.subTotal, 
+      }],
       deliveryInfo: {
         deliveryCost: 0,
         deliveryRemarks: '',
@@ -80,37 +84,39 @@ function transformOrder(source) {
 
 
 async function executePushOrder(order, paymentDetails, pos) {
-  const target = transformOrder(order);
+  const target = transformOrder(order, pos);
   if (target === null) {
     throw new Error({code: 1, message: 'failure in transfromOrder'});
   }
 
   // todo: for now we always say 'OK' and send the confirmation e-mail to '7739985@gmail.com' (seba)
 
-  // let headers = {
-  //     'Content-Type': 'application/json',
-  //     'access_token': access_token
-  // };
+  
 
-  // let options = {
-  //     host: host,
-  //     port: port,
-  //     path: pushOrderResource,
-  //     method: 'POST',
-  //     headers: headers
-  // };
+  let headers = {
+      'Content-Type': 'application/json',
+      'access_token': access_token
+  };
 
-  // let req = https.request(options, function(res) {
-  //     res.setEncoding('utf8');
-  //     res.on('data', function (chunk) {
-  //         console.log('pushed order to ' + pushOrderResource);
-  //         console.log('response: ' + chunk);
-  //         callback(undefined, {code: res.statusCode, message: chunk});
-  //     });
-  // });
+  let options = {
+      host: providerConf.host,
+      port: providerConf.port,
+      path: pushOrderResource,
+      method: 'POST',
+      headers: headers
+  };
 
-  // req.write(JSON.stringify(target));
-  // req.end();
+  let req = https.request(options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+          console.log('pushed order to ' + pushOrderResource);
+          console.log('response: ' + chunk);
+          // callback(undefined, {code: res.statusCode, message: chunk});
+      });
+  });
+
+  req.write(JSON.stringify(target));
+  req.end();
 
   return {
     code: '200',
